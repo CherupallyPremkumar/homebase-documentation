2e9ff82cabac03a283d10f562342f41a966199f3
Prem Kumar Cherupally <premkumar.cherupally@gmail.com>
2025-12-19 13:58:10 -0600

    fix: Make all content full width, not just banner

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ function App() {
         />
       </div>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-8">
+      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 pb-8">
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
           <Sidebar
             documents={filteredDocs}
